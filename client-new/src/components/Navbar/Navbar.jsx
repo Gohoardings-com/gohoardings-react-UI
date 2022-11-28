16f81fbe618f57e93e8f61e081b6e7561408bfcb
goh-uday <uday@gohoardings.com>
2022-11-28 11:16:54 +0530

    11:16

---
+++ client-new/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import Nav from "react-bootstrap/Nav";
+import Dropdown from 'react-bootstrap/Dropdown';
+import Navbar from "react-bootstrap/navbar";
+import './navbar.scss'
+import { MdLocationOn } from 'react-icons/md'
+import Drop_Down_Image from "../drop_drown/drop_down_image";
+import UserDetail from "./userDetail";
+
+const NewNAvbar = () => {
+  const [show, setShow] = useState(false);
+  const [posts, setPosts] = useState()
+  
+useEffect(() => {
+  setPosts(posts)
+}, [posts]);
+  return (
+    <>
+      <Navbar expand="lg px-md-0 pb-0 fixd-nabar">
+        <div className="navbar container-fluid py-md-0 ">
+          <Navbar.Brand >
+            <img src="./images/logo.png" className="brand ms-sm-5 ms-3" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="me-3" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className=" ms-auto me-5">
+              <Nav.Link
+                className="navLink ps-3 text-dark fw-bold"
+                href="https://odoads.com/"
+                target="_blank"
+              >
+                Odoads
+              </Nav.Link>
+              <Nav.Link
+                className="navLink ps-3 text-dark fw-bold"
+                href="https://www.gohoardings.com/blog/"
+                target="_blank"
+              >
+                Blog
+              </Nav.Link>
+              <Nav.Link
+                className="navLink ps-3 pe-3 fw-bold text-dark"
+                href="/contact"
+              >
+                Contact
+              </Nav.Link>
+              <Nav.Link
+                className="mapLink font-map-btn text-white  text-nowrap rounded-pill "
+                href="/map"
+              >
+                <MdLocationOn className="ps-0 p-0  pb-1 font-map-logo" />
+                Map View
+              </Nav.Link>
+              <UserDetail posts={posts} setPosts={setPosts}/>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+      <Drop_Down_Image show={show} setShow={setShow} Dropdown={Dropdown} />
+    </>
+  );
+};
+
+export default NewNAvbar;
+
