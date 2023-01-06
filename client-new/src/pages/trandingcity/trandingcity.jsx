433de19a65fbb49f900b2dbd489fbd7b87a07445
ishwar <ishwarthaku250@gmail.com>
2023-01-06 17:41:00 +0530

    gfyuuuuuuuuuuuuuuuuu

--- client-new/src/pages/trandingcity/trandingcity.jsx
+++ client-new/src/pages/trandingcity/trandingcity.jsx
@@ -106,7 +106,7 @@ if(!loading){
                         
                           <div className="bottom-left">Delhi</div>
                           <div className="bottom-left-media">
-                            {pos.medianame.substring(17, 30)}{" "}
+                            {pos.medianame.substring(0, 17)}...
                           </div>
                         </div>
                       </Link>
