104097e2393a5d033184820c299c605d8bdf829f
goh-deepak <rickygathwal@gmail.com>
2023-01-07 18:22:14 +0530

    mgjmm

--- client-new/src/pages/enquire/enquireregister.jsx
+++ client-new/src/pages/enquire/enquireregister.jsx
@@ -20,27 +20,27 @@ const Enquireregister = () => {
     const onSubmit = async (e) => {
 
         e.preventDefault();
-        const emailformate = /^\w+([-]?\w+)*@\w+(.-]?\w+)*(\.\w{2,3})+$/;
-        let count = 0;
-        if (name === "") {
-            count = +1;
-            setNameValidate(<MdOutlineError className="text-danger" />);
-        } else if (number.length <= 0) {
-            count = +1;
-            setNumbervalidate(<MdOutlineError className="text-danger" />);
-        } else if (number.length !== 10) {
-            count = +1;
-            setNumbervalidate("Type your 10 digit number corectly");
-        } else if (email === "") {
-            count = +1;
-            setEmailValidate(<MdOutlineError className="text-danger" />);
-        } else if (!emailformate.test(email)) {
-            count = +1;
-            setEmailValidate("Type your email corectly");
-        } else if (message === "") {
-            count = +1;
-            setMessageValidate(<MdOutlineError className="text-danger" />);
-        } else if (count === 0) {
+        // const emailformate = /^\w+([-]?\w+)*@\w+(.-]?\w+)*(\.\w{2,3})+$/;
+        // let count = 0;
+        // if (name === "") {
+        //     count = +1;
+        //     setNameValidate(<MdOutlineError className="text-danger" />);
+        // } else if (number.length <= 0) {
+        //     count = +1;
+        //     setNumbervalidate(<MdOutlineError className="text-danger" />);
+        // } else if (number.length !== 10) {
+        //     count = +1;
+        //     setNumbervalidate("Type your 10 digit number corectly");
+        // } else if (email === "") {
+        //     count = +1;
+        //     setEmailValidate(<MdOutlineError className="text-danger" />);
+        // } else if (!emailformate.test(email)) {
+        //     count = +1;
+        //     setEmailValidate("Type your email corectly");
+        // } else if (message === "") {
+        //     count = +1;
+        //     setMessageValidate(<MdOutlineError className="text-danger" />);
+        // } else if (count === 0) {
           await instance.post("enquiry/message", { name, email, number, message });
            
             notify();
@@ -53,7 +53,7 @@ const Enquireregister = () => {
             setEmailValidate("");
             setMessageValidate("");
 
-        }
+        // }
     };
 
     const notify = async () => {
@@ -64,42 +64,42 @@ const Enquireregister = () => {
     <h1 className="txt-clr-tlk">Talk to us!</h1>
             <h6 className="txt-clr" >*Please fill all the details.</h6>
             <form className='mt-4 "position-relative' onSubmit={onSubmit}>
-  <div className="form-group py-3 ">
-    <label htmlFor="formGroupExampleInput">Name*</label>
-    <input type="text" className="form-control ps-0 rounded-0" id="formGroupExampleInput" placeholder="Your Full Name" value={name}
+  <div class="form-group py-3 ">
+    <label for="formGroupExampleInput">Name*</label>
+    <input type="text" class="form-control ps-0 rounded-0" id="formGroupExampleInput" placeholder="Your Full Name" value={name}
                 onChange={(e) => {
                     setName(e.target.value);
                 }}/>
                   <p className="ms-2 p-0 text ">{nameValidate}</p>
   </div>
-  <div className="row py-3">
-  <div className="col">
-    <label htmlFor="Last-name">Email*</label>
-      <input type="text" className="form-control ps-0 rounded-0" placeholder="Your Mail ID" id='first-name'   value={email}
+  <div class="row py-3">
+  <div class="col">
+    <label for="Last-name">Email*</label>
+      <input type="text" class="form-control ps-0 rounded-0" placeholder="Your Mail ID" id='first-name'   value={email}
                 onChange={(e) => {
                     setEmail(e.target.value);
                 }}/>
                   <p className="ms-2 p-0 text ">{emailValidate}</p>
     </div>
-    <div className="col">
-    <label htmlFor="Last-name">Phone Number*</label>
-      <input type="text" className="form-control ps-0 rounded-0" placeholder="+1 012 3456 789" id='+1 012 3456 789'  value={number}
+    <div class="col">
+    <label for="Last-name">Phone Number*</label>
+      <input type="text" class="form-control ps-0 rounded-0" placeholder="+1 012 3456 789" id='+1 012 3456 789'  value={number}
                 onChange={(e) => {
                     setNumber(e.target.value);
                 }}/>
                   <p className="ms-2 p-0 text ">{numbervalidate}</p>
     </div>
   </div>
-  <div className="form-group py-3">
-    <label htmlFor="formGroupExampleInput2">Message*</label>
-    <input type="text" className="form-control ps-0 rounded-0" id="formGroupExampleInput2" placeholder="Write your message.."   value={message}
+  <div class="form-group py-3">
+    <label for="formGroupExampleInput2">Message*</label>
+    <input type="text" class="form-control ps-0 rounded-0" id="formGroupExampleInput2" placeholder="Write your message.."   value={message}
                 onChange={(e) => {
                     setMessage(e.target.value);
                 }}/>
            <p className="ms-2 p-0 text ">{messageValidate}</p>     
   </div>
  <div className=' p-0 m-0 position-relative '>
- <button type="submit" className="btn btn-lg  message-btn  float-end" role="button">Send Message</button>
+ <button type="submit" class="btn btn-lg  message-btn  float-end" role="button">Send Message</button>
  <ToastContainer/>
  </div>
  
--- server/controller/enquiry.js
+++ server/controller/enquiry.js
@@ -1,24 +1,20 @@
 const db = require("../conn/conn");
 const catchError = require("../middelware/catchError");
 
-exports.message = catchError(async (req, res, next) => {
-    try {
+exports.message = catchError(async (req, res) => {
+    console.log("agdscgah");
+   
         const {name, email, phone, message} = req.body
         db.changeUser({database: "sql_login"})
-        req.body ? db.query("INSERT into enquiry (name, email, phone, message) VALUES ('" + name + "', '" + email + "', '" + phone + "','" + message + "')", async (err, result) => {
+        db.query("INSERT into enquiry (name, email, phone, message) VALUES ('" + name + "', '" + email + "', " + phone + ",'" + message + "')", async (err, result) => {
                 if (err) {
+                    console.log(err);
                     return res.send({err: err, message: "Something Wrong here"})
-                } else if (result == []) {
-                    res.send({"err": err, message: "Something Wrong here"})
                 } else {
+                    console.log(result);
                     return res.send({result: result, message: "Login Successfully"})
                 }
             })
-            : res.send({message: "User data Null"})
-    } catch (err) {
-        res.status(204).json({
-            messsage: err.res
-        })
-    }
+    
 })
 
